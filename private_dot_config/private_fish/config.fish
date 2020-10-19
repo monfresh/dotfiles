d3d1d823a5e4c5a2ab9ca554550e5a175b73e87c
Moncef Belyamani <moncef@example.com>
2020-10-18 22:19:29 -0400

    Update Ruby to 2.7.2

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -6,6 +6,6 @@ source /usr/local/share/chruby/chruby.fish
 
 source /usr/local/share/chruby/auto.fish
 
-chruby ruby-2.7.1
+chruby ruby-2.7.2
 
 status --is-interactive; and source (nodenv init -|psub)
