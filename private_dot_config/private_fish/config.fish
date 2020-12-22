8418ca8eb8263b154e57c6860eeb0f6cf784f9ae
Moncef Belyamani <moncef@example.com>
2020-12-22 09:32:37 -0500

    Remove chruby-fish lines from config.fish
    It's buggy. Need to use this fork until it gets fixed: https://github.com/bouk/chruby-fish/tree/rewrite-fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -2,10 +2,6 @@ source ~/.fish_aliases
 
 source ~/.fish_variables
 
-source /usr/local/share/chruby/chruby.fish
-
-source /usr/local/share/chruby/auto.fish
-
 chruby ruby-2.7.2
 
 status --is-interactive; and source (nodenv init -|psub)
