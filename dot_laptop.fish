f0f65ac9c0e3913db345202fadd96d8f15edf974
Moncef Belyamani <moncef@example.com>
2020-12-22 09:35:48 -0500

    Set up chruby-fish

--- dot_laptop.fish
+++ dot_laptop.fish
@@ -11,3 +11,10 @@ if [ ! -d "$HOME/.local/share/omf" ]; then
   fancy_echo "installing omf ..."
   curl -L https://get.oh-my.fish | fish
 fi
+
+# Set up chruby-fish using fork until this PR is merged:
+# https://github.com/JeanMertz/chruby-fish/pull/39
+gh repo clone bouk/chruby-fish
+cd chruby-fish
+git checkout -b rewrite-fish origin/rewrite-fish
+make install
