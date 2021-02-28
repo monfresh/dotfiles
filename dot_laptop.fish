ee5491aec255545c6ea77c8d4400533d1f09614e
Moncef Belyamani <gither@fastmail.fm>
2021-02-27 21:08:25 -0500

    Add support for M1 Apple Silicon
    Homebrew installs in a different location when using native M1 chip vs Rosetta mode

--- dot_laptop.fish
+++ dot_laptop.fish
@@ -3,8 +3,13 @@
 # Add fish to /etc/shells and change to fish
 if ! grep -qs "fish" /etc/shells; then
  fancy_echo "Adding fish to /etc/shells ..."
- echo "/usr/local/bin/fish" | sudo tee -a /etc/shells
- chsh -s /usr/local/bin/fish
+ if apple_m1 && ! rosetta; then
+   echo "/opt/homebrew/bin/fish" | sudo tee -a /etc/shells
+   chsh -s /opt/homebrew/bin/fish
+ else
+   echo "/usr/local/bin/fish" | sudo tee -a /etc/shells
+   chsh -s /usr/local/bin/fish
+ fi
 fi
 
 if [ ! -d "$HOME/.local/share/omf" ]; then
