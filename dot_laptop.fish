ca00b0bbeb886a99e69ea029d81f51154ea7b079
Moncef Belyamani <gither@fastmail.fm>
2022-07-01 19:25:37 -0400

    Simplify setting fish shell

--- dot_laptop.fish
+++ dot_laptop.fish
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-# Add fish to /etc/shells and change to fish
+# Add fish to /etc/shells and change shell to fish
 if ! grep -qs "fish" /etc/shells; then
  fancy_echo "Adding fish to /etc/shells ..."
- if apple_m1 && ! rosetta; then
-   echo "/opt/homebrew/bin/fish" | sudo tee -a /etc/shells
-   chsh -s /opt/homebrew/bin/fish
- else
-   echo "/usr/local/bin/fish" | sudo tee -a /etc/shells
-   chsh -s /usr/local/bin/fish
- fi
+ echo "$(brew --prefix)/bin/fish" | sudo tee -a /etc/shells
+ chsh -s "$(brew --prefix)/bin/fish"
 fi
