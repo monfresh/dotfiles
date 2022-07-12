d1ce698e041d35ffe446c9872a0634b98a0365f1
Moncef Belyamani <gither@fastmail.fm>
2022-07-11 21:48:01 -0400

    Use fish setup from Ruby on Mac Ultimate
    https://www.rubyonmac.dev/ultimate

--- dot_laptop.fish
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Add fish to /etc/shells and change shell to fish
-if ! grep -qs "fish" /etc/shells; then
- fancy_echo "Adding fish to /etc/shells ..."
- echo "$(brew --prefix)/bin/fish" | sudo tee -a /etc/shells
- chsh -s "$(brew --prefix)/bin/fish"
-fi
